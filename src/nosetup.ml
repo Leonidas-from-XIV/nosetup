7afc1c78f5b436d075dce7b02a51f6634ed047e3
Marek Kubica <marek.kubica@stylefruits.de>
2016-12-29 12:23:52 +0100

    Implement recursive search for merlin file

--- src/nosetup.ml
+++ src/nosetup.ml
@@ -1,16 +1,29 @@
 open Containers
 (* TODO: add support for _tags file as well *)
 
+let rec locate_till_root path filename =
+  let candidate = Filename.concat path filename in
+  match Unix.access candidate [Unix.R_OK] with
+  | _ -> Some candidate
+  | exception Unix.Unix_error _ ->
+    let parent = Filename.concat path Filename.parent_dir_name in
+    match (Unix.stat path, Unix.stat parent) with
+    | ({ Unix.st_ino = this_ino}, { Unix.st_ino = parent_ino }) when this_ino = parent_ino ->
+        None
+    | _ -> locate_till_root parent filename
+
 let locate_merlin_file path =
-  ".merlin"
+  locate_till_root path ".merlin"
 
 let parse_merlin_packages path =
-  let content = IO.with_in path IO.read_all in
-  Merlin_parser.parse content
+  IO.with_in path IO.read_all
+  |> Merlin_parser.parse
 
 let main () =
-  locate_merlin_file "."
-  |> parse_merlin_packages
+  let open Option.Infix in
+  locate_merlin_file Filename.current_dir_name
+  >|= parse_merlin_packages
+  |> Option.get_or ~default:[]
   |> Topfind.load_deeply
 
 let () =
