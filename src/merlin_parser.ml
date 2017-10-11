217b5b8f59a1253db3c140729630c5c06f2752ab
Ralf Vogler <ralf.vogler@gmail.com>
2017-10-11 15:38:45 +0200

    support parsing multiple packages per line

--- src/merlin_parser.ml
+++ src/merlin_parser.ml
@@ -1,21 +1,19 @@
 open Angstrom
 
 let is_not_end_line c = c <> '\n'
+let pkg_name c = c <> ' ' && c <> '\n'
 
-let pkg_line = string "PKG " *> take_while1 is_not_end_line >>= fun pkg -> return @@ Some pkg
-let skip_line = skip_while is_not_end_line >>= fun () -> return None
-
-let linebreak = char '\n'
+let pkg_line = string "PKG " *> sep_by (char ' ') (take_while1 pkg_name)
+let skip_line = skip_while is_not_end_line *> return []
 
 let any_line = pkg_line <|> skip_line
 
-let parser = sep_by linebreak any_line
+let parser = sep_by (char '\n') any_line
 
 let parse content =
+  print_endline "hallo";
   match parse_only parser (`String content) with
   | Result.Error msg ->
     []
   | Result.Ok lines ->
-    lines
-    |> List.filter CCOpt.is_some
-    |> List.map CCOpt.get_exn
+    List.flatten lines
